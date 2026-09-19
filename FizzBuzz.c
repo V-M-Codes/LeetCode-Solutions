7a0e57ca71bbe6f85e434ca6214deb16d57956a0
Varsha M <varsha17806@gmail.com>
2026-09-19 12:39:08 +0530

    Problem 412

---
+++ FizzBuzz.c
@@ -0,0 +1,52 @@
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 104
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    *returnSize = n; 
+    char **answer = (char **)malloc(n * sizeof(char *));
+
+    for(int i = 1; i <= n; i++) {
+        answer[i - 1] = (char *)malloc(15 * sizeof(char));
+
+        if(i % 3 == 0 && i % 5 == 0) {
+            sprintf(answer[i - 1], "FizzBuzz");
+        }
+        else if(i % 3 == 0) {
+            sprintf(answer[i - 1], "Fizz");
+        }
+        else if(i % 5 == 0) {
+            sprintf(answer[i - 1], "Buzz");
+        }
+        else {
+            sprintf(answer[i - 1], "%d", i);
+        }
+    }
+
+    return answer;
+    
+}
